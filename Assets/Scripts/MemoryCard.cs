7eff259302a5bc44698d021896c56d840dcf7c7f
lucas gray <lucas@beekeeperdata.com>
2016-02-28 19:59:14 -0600

    memory

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MemoryCard : MonoBehaviour {
+	
+	[SerializeField] private SceneController controller;
+	[SerializeField] private GameObject cardBack;
+
+	private int _id;
+	public int id {
+		get { return _id; }
+	}
+
+	public void SetCard(int id, Sprite image) {
+		_id = id;
+		Debug.Log ("Setting card");
+		Debug.Log (id);
+		GetComponent<SpriteRenderer>().sprite = image;
+	}
+
+	public void OnMouseDown() {
+		if (cardBack.activeSelf && controller.canReveal) {
+			cardBack.SetActive (false);
+			controller.CardRevealed (this);
+		}
+	}
+
+	public void Unreveal() {
+		cardBack.SetActive (true);
+	}
+
+}
